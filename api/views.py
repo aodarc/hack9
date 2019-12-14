c3b63c821985cc650898d6f24900cfd5c4118a56
o.odarchenko <o.odarchenko@levi9.com>
2019-12-14 04:51:50 +0200

    added post invoice

--- api/views.py
+++ api/views.py
@@ -271,7 +271,7 @@ async def listing(calling: str, request: Request):
     return response
 
 
-# @app.post("/financial/invoice", status_code=202)
+@app.post("/financial/invoice", status_code=202)
 async def financial_invoice(invoice: InvoiceData):
     """
     :description:
