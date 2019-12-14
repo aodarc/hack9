46039718cd9f3929083a7f64a5dadff76c8896c3
rudy <m.shchetinin@levi9.com>
2019-12-14 10:43:21 +0200

    another approach for round.

--- api/utils/cost_calculation.pyx
+++ api/utils/cost_calculation.pyx
@@ -1,5 +1,8 @@
-from libc.math cimport ceil
+from libc.math cimport ceil, round
 
 
 def calculate_cost(int initial, int duration, float increment, float rate):
-    return ((ceil((initial + duration) / increment) * increment) * (rate / 60.0)) // 0.01 / 100
+    return round(((ceil((initial + duration) / increment) * increment) * (rate / 60.0)) * 100) / 100
+
+def calculate_round(int initial, int duration, float increment):
+    return ceil((initial + duration) / increment) * increment
